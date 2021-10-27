fb2859c75134823583dc349df366ecc82fc6515a
Adi Levy-Alalouf <blue.vixen@gmail.com>
2021-10-27 19:50:35 +0300

    added line operations buttons and failed drag and drop

--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,17 @@
 'use strict'
 
+const gTouchEvs = ['touchstart', 'touchmove', 'touchend']
+
 function onInit() {
     console.log('lets meme!')
     gElCanvas = document.getElementById('my-canvas')
     gCtx = gElCanvas.getContext('2d')
     gCtx.fillStyle = 'white'
     gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
+    gMeme.lines[0].pos = { x: (gElCanvas.width / 2), y: 50 }
     renderCanvas()
     renderGallery();
+    // addListeners()
 }
 
 
@@ -15,20 +19,11 @@ function renderCanvas() {
     if (!gCurrImg) {
         gCtx.fillStyle = "#ede5ff"
         gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
+        drawText();
     } else renderImage(gCurrImg)
     gCtx.save()
     gCtx.restore()
-}
-
-function renderMeme() {
-    var img = new Image()
-    img.src = getImage();
-    // console.log(img)
-    img.onload = () => {
-        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
-        drawText();
-    }
-
+    drawText();
 }
 
 
@@ -43,13 +38,21 @@ function renderImage(url) {
 
 function drawText() {
     var txt = getTxt();
+    var fill = getFill();
+    var stroke = getStroke();
+    var fontSize = getFontSize();
+    var font = getFont();
+    const { pos, align } = gMeme.lines[0]
     gCtx.lineWidth = 2;
-    gCtx.strokeStyle = 'black';
-    gCtx.fillStyle = 'white';
-    gCtx.font = '40px Impact';
-    gCtx.fillText(txt.toUpperCase(), 50, 50);
-    gCtx.strokeText(txt.toUpperCase(), 50, 50);
-    gCtx.stroke();
+    gCtx.strokeStyle = stroke;
+    gCtx.fillStyle = fill;
+    gCtx.font = `${fontSize}px ${font}`;
+    gCtx.textAlign = align;
+    // var textWidth = gCtx.measureText(txt).width;
+    // var textHeight = (gCtx.measureText(txt).fontBoundingBoxDescent) + (gCtx.measureText(txt).fontBoundingBoxAscent)
+    // console.log(textHeight);
+    gCtx.fillText(txt.toUpperCase(), pos.x, pos.y);
+    gCtx.strokeText(txt.toUpperCase(), pos.x, pos.y);
 }
 
 
@@ -75,4 +78,77 @@ function renderGallery() {
 function updateCanvas(imgId) {
     var img = getImageById(imgId);
     renderImage(img.url)
+}
+
+function onIncreaseFont() {
+    increaseFont();
+    renderCanvas();
+}
+
+function onDecreaseFont() {
+    decreaseFont();
+    renderCanvas();
+}
+
+/* Drag and Drop - currently disabled */
+
+
+function addListeners() {
+    addMouseListeners()
+    addTouchListeners()
+}
+
+function addMouseListeners() {
+    gElCanvas.addEventListener('mousemove', onMove)
+    gElCanvas.addEventListener('mousedown', onDown)
+    gElCanvas.addEventListener('mouseup', onUp)
+}
+
+function addTouchListeners() {
+    gElCanvas.addEventListener('touchmove', onMove)
+    gElCanvas.addEventListener('touchstart', onDown)
+    gElCanvas.addEventListener('touchend', onUp)
+}
+
+
+
+function onDown(ev) {
+    const pos = getEvPos(ev)
+    if (!isTextClicked(pos)) return
+    // console.log(isTextClicked(pos))
+    gMeme.lines[0].isDrag = true;
+    gStartPos = pos
+    document.body.style.cursor = 'grabbing'
+}
+
+function onMove(ev) {
+    const line = gMeme.lines[0];
+    if (line.isDrag) {
+        const pos = getEvPos(ev);
+        const dx = pos.x - gStartPos.x
+        const dy = pos.y - gStartPos.y
+        gStartPos = pos
+        renderCanvas()
+    }
+}
+
+function onUp() {
+    gMeme.lines[0].isDrag = false;
+    document.body.style.cursor = 'default'
+}
+
+function getEvPos(ev) {
+    var pos = {
+        x: ev.offsetX,
+        y: ev.offsetY
+    }
+    if (gTouchEvs.includes(ev.type)) {
+        ev.preventDefault()
+        ev = ev.changedTouches[0]
+        pos = {
+            x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+            y: ev.pageY - ev.target.offsetTop - ev.target.clientTop
+        }
+    }
+    return pos
 }
--- js/memeService.js
+++ js/memeService.js
@@ -3,6 +3,7 @@
 var gElCanvas
 var gCtx
 var gCurrImg = ''
+var gStartPos
 
 var gImgs = [
     {
@@ -24,8 +25,12 @@ var gMeme = {
         {
             txt: '',
             size: 20,
-            align: 'left',
-            color: 'red'
+            align: 'center',
+            color: 'white',
+            stroke: 'black',
+            font: 'Impact',
+            pos: { x: 0, y: 0 },
+            isDrag: false,
 
         }
     ]
@@ -35,10 +40,6 @@ function getImages() {
     return gImgs;
 }
 
-
-function getImage() {
-    return gImgs[0].url;
-}
 
 function getImageById(imgId) {
     var currImage = gImgs.find(image => image.id === imgId);
@@ -50,6 +51,43 @@ function getTxt() {
     return gMeme.lines[0].txt;
 }
 
+function getFill() {
+    return gMeme.lines[0].color;
+}
+
+function getStroke() {
+    return gMeme.lines[0].stroke;
+}
+
+function getFontSize() {
+    return gMeme.lines[0].size;
+}
+
+function getFont() {
+    return gMeme.lines[0].font;
+}
+
 function updateText(text) {
     gMeme.lines[0].txt = text;
+}
+
+function increaseFont() {
+    gMeme.lines[0].size += 10;
+    console.log(gMeme.lines[0].size)
+}
+
+function decreaseFont() {
+    gMeme.lines[0].size -= 10;
+    console.log(gMeme.lines[0].size)
+}
+
+function isTextClicked(clickedPos) {
+    const { pos, size, txt } = gMeme.lines[0];
+    // console.log(gCtx.measureText(txt).width);
+    return ((clickedPos.x >= pos.x && clickedPos.x <= pos.x + gCtx.measureText(txt).width)) && ((clickedPos.y <= pos.y && clickedPos.y >= pos.y - size))
+}
+
+function moveLine(dx, dy) {
+    gMeme.lines[0].pos.x = dx;
+    gMeme.lines[0].pos.y = dy;
 }
